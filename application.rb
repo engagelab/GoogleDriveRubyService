09ec70fdc7e4eade213e6696964fc012b086288d
labrat <labrat@imediamac45.local>
2013-04-09 08:46:00 +0200

    heroku ready

--- application.rb
+++ application.rb
@@ -1,32 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require "google_drive"
-require 'digest'
 require 'json'
-require 'fileutils'
-require 'logger'
 
-set :port, 4568
-
-Dir.mkdir('logs') unless File.exist?('logs')
-$log = Logger.new('logs/output.log')
-
-configure do
-    $log.level = Logger::DEBUG
-end
+$KCODE = "UTF-8"
+#set :port, 4568
 
 post '/gdriveFiles' do
 	request.body.rewind  # in case someone already read it
 	content_type :json;
 	data = JSON.parse request.body.read
+	p data['folderName']
 	
-	$log.debug 'folder requested: '+data['folderName'];
-	
-	session = GoogleDrive.login("intermedia.miracle@gmail.com", "vaffel123");
-	#this queries the folder called 'heatpump'
+	session = GoogleDrive.login("intermedia.miracle@gmail.com", "labrat123");
 	@folderHierarchy = session.collection_by_url('https://docs.google.com/feeds/default/private/full/folder%3A0B0ISH1Jh_26pSE1tdjE2RWlhM0E?v=3');
-	
-	#$log.debug @folderHierarchy.files;
 	
 	fileURLs = [];
 	
